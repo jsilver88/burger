844a532a326b71696856291e85d158f65cfb3263
Jared Silver <jrsilver88@gmail.com>
2020-05-15 16:04:49 -0700

    on click functions added when user enters a new burger and devour it.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,8 +15,8 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burgers.insertOne(["burger_name"], [req.body.burger_name], function (res) {
-        res.json({ id: res.insertId });
+    burger.insertOne(["burger_name"], [req.body.burger_name], function (result) {
+        res.json({ id: result.insertId });
     });
 });
 
@@ -27,8 +27,8 @@ router.put("/api/burgers/:id", function (req, res) {
 
     burger.updateOne({
         devoured: 1
-    }, condition, function (res) {
-        if (res.changedRows === 0) {
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
             return res.status(404).end();
         } else {
             res.status(200).end();
--- public/assets/js/burgerLogic.js
+++ public/assets/js/burgerLogic.js
@@ -1,3 +1,33 @@
 $(function () {
-    $("")
+    $(".burger-form").on("submit", function (e) {
+        e.preventDefault();
+
+        let userBurger = {
+            burger_name: $("#newBurger").val().trim()
+        }
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: userBurger
+        }).then(function () {
+            console.log("User's burger added.")
+            location.reload();
+        })
+    });
+
+    $(".devoured").on("click", function (e) {
+        console.log(e);
+
+        let id = $(this).data("id");
+        let newBurger = {
+            devoured: 0
+        }
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newBurger
+        }).then(function () {
+            console.log("New Burger updated.")
+            location.reload();
+        })
+    })
 })
